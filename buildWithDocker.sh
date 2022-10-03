1066c0fe6b3ac75060e2c981f0649e72eea19e66
Christian Stocker <me@chregu.tv>
2022-10-03 21:06:50 +0200

    dashboard stuff is not needed here anymore

--- buildWithDocker.sh
+++ buildWithDocker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # please dont forget to run ./docker/build.sh in case you don't have the image yet
-docker run --rm -v $PWD:/root/deploy -v rokka-site-node_modules:/root/deploy/node_modules -v rokka-site-dashboard:/root/deploy/rokka-dashboard docker.gitlab.liip.ch/rokka/rokka-site-build-docker:latest ./docker/buildDeployment.sh
+docker run --rm -v $PWD:/root/deploy -v rokka-site-node_modules:/root/deploy/node_modules docker.gitlab.liip.ch/rokka/rokka-site-build-docker:latest ./docker/buildDeployment.sh
 
 
 
--- docker/buildDeployment.sh
+++ docker/buildDeployment.sh
@@ -5,13 +5,5 @@ nvm use $NODE_JS_VERSION;
 rm -rf dist/*
 
 npm install
-if [[ ! -d rokka-dashboard/.git ]]; then
-  echo "rokka-dashboard/.git did not exist, clone repo"
-  find rokka-dashboard -maxdepth 1 -mindepth 1  -print0 |   xargs -0 -i rm -rf {}
-  git clone -q https://github.com/rokka-io/rokka-dashboard.git rokka-dashboard
-else
-  cd rokka-dashboard
-  git pull
-  cd ..
-fi
+
 ./node_modules/.bin/gulp build:prod
