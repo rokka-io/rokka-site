81aebbf49c2aab0839384476f1c2896e338f0269
Christian Stocker <me@chregu.tv>
2022-10-03 21:12:05 +0200

    make docker images more explicit for a later upgrade

--- docker/build.sh
+++ docker/build.sh
@@ -2,4 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-docker build -t docker.gitlab.liip.ch/rokka/rokka-site-build-docker .
+docker build -t docker.gitlab.liip.ch/rokka/rokka-site-build-docker:node10 .
